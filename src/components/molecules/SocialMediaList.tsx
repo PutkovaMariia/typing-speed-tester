99fb30603be8d3f0801c73b03115306acfba0332
Mariia Putkova <mashaputkova@gmail.com>
2025-08-17 07:40:10 +0200

    added social medias

---
+++ src/components/molecules/SocialMediaList.tsx
@@ -0,0 +1,27 @@
+import socialMediaData from "@/const/socialMediaData.json";
+import Link from "next/link";
+
+export default function SocialMediaList() {
+    return (
+        <ul className="flex items-center justify-center lg:justify-end gap-7">
+            {socialMediaData.map(({link, label, icon}) => {
+                const src = `/img/contact-icons/contact-${icon}`;
+                return (
+                    <li key={label}>
+                        <Link
+                            href={link}
+                            aria-label={label}
+                            style={{
+                                WebkitMaskImage: `url(${src})`,
+                                maskImage: `url(${src})`,
+                            }}
+                            className="block size-10 cursor-pointer transition bg-gray-500 hover:scale-115 [mask-size:contain] [mask-repeat:no-repeat] [mask-position:center]"
+                        >
+                            <span className="sr-only">{label}</span>
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    )
+}
