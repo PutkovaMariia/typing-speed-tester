c2f69799d12ad7abe480d3e5612ce4802ad232e2
Mariia Putkova <mashaputkova@gmail.com>
2025-08-17 10:12:25 +0200

    refactored header style

--- src/components/organizms/Header.tsx
+++ src/components/organizms/Header.tsx
@@ -2,7 +2,7 @@ import { content } from "@/const/contentTyped";
 
 export default function Header() {
   return (
-    <header className="w-full max-w-3xl mx-auto text-center pt-10">
+    <header className="w-full max-w-3xl mx-auto text-center pt-3 lg:pt-10">
       <h1
         className="font-dancingScript text-5xl lg:text-7xl font-extrabold tracking-tight
                      text-transparent bg-clip-text bg-gradient-to-r
